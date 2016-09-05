e5ab87a04bbf95f4e4f988110c6e670a1f312810
fernandox7 <framirez1997@gmail.com>
2016-09-04 21:31:30 -0500

    Add login screen

--- app/login/login-controller.js
+++ app/login/login-controller.js
@@ -6,8 +6,13 @@ angular.module('techNews').controller('LoginController', function ($scope) {
     vm = this;
 
     // Public functions
+    vm.login = login;
 
     // Public properties
     vm.hello = 'Hello there';
+
+    function login() {
+        console.log('Logging in...');
+    }
 
 });
