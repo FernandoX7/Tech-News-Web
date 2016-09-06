c8f4bf42fd69d395906b180c7c77f00acec7f682
fernandox7 <framirez1997@gmail.com>
2016-09-05 23:02:43 -0500

    Add timestamp service
    Update controllers using old timestamp ways

--- app/login/register-controller.js
+++ app/login/register-controller.js
@@ -1,13 +1,12 @@
 /**
  * Created by fernandoramirez on 9/4/16.
  */
-angular.module('techNews').controller('RegisterController', function () {
+angular.module('techNews').controller('RegisterController', function (timestampService) {
 
     var vm = this;
 
     // Public functions
     vm.register = register;
-    vm.getTimestamp = getTimestamp;
 
     // Public properties
     vm.firstName = '';
@@ -22,7 +21,7 @@ angular.module('techNews').controller('RegisterController', function () {
         lastName = vm.lastName;
         email = vm.email;
         password = vm.password;
-        var timestamp = getTimestamp();
+        var timestamp = timestampService.getTimestamp();
 
         // Create the user and save them to firebase
         firebase.auth().createUserWithEmailAndPassword(email, password).then(function (user) {
@@ -69,33 +68,6 @@ angular.module('techNews').controller('RegisterController', function () {
                 }
             });
 
-    }
-
-    // Get Date
-    function getTimestamp() {
-        // Expected result = September 1, 2016 9:13:34 PM
-        var objToday = new Date(),
-            weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
-            dayOfWeek = weekday[objToday.getDay()],
-            domEnder = function () {
-                var a = objToday;
-                if (/1/.test(parseInt((a + "").charAt(0)))) return "th";
-                a = parseInt((a + "").charAt(1));
-                return 1 == a ? "st" : 2 == a ? "nd" : 3 == a ? "rd" : ""
-            }(),
-            dayOfMonth = today + ( objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder : objToday.getDate() + domEnder,
-            months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
-            curMonth = months[objToday.getMonth()],
-            curYear = objToday.getFullYear(),
-            curHour = objToday.getHours() > 12 ? objToday.getHours() - 12 : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours()),
-            curMinute = objToday.getMinutes() < 10 ? "0" + objToday.getMinutes() : objToday.getMinutes(),
-            curSeconds = objToday.getSeconds() < 10 ? "0" + objToday.getSeconds() : objToday.getSeconds(),
-            curMeridiem = objToday.getHours() > 12 ? "PM" : "AM";
-
-        var timestamp = curMonth + ' ' + dayOfMonth + ', ' + curYear + ' ' + curHour + ":" + curMinute + ":" + curSeconds + ' ' + curMeridiem;
-        var today = curHour + ":" + curMinute + "." + curSeconds + curMeridiem + " " + dayOfWeek + " " + dayOfMonth + " of " + curMonth + ", " + curYear;
-
-        return timestamp
     }
 
 });
--- app/new-article/new-article-controller.js
+++ app/new-article/new-article-controller.js
@@ -1,7 +1,7 @@
 /**
  * Created by fernandoramirez on 9/4/16.
  */
-angular.module('techNews').controller('NewArticleController', function (getLoggedInUser, $timeout) {
+angular.module('techNews').controller('NewArticleController', function (getLoggedInUser, $timeout, timestampService) {
 
     var vm = this;
 
@@ -9,7 +9,6 @@ angular.module('techNews').controller('NewArticleController', function (getLogge
     vm.init = init;
     vm.getCurrentUser = getCurrentUser;
     vm.createArticle = createArticle;
-    vm.getTimestamp = getTimestamp;
 
     // Public properties
     vm.user = {}; // Update the properties with the users - if they are logged in
@@ -24,7 +23,7 @@ angular.module('techNews').controller('NewArticleController', function (getLogge
     function createArticle(uid, author, authorReference, title, description, image, timestamp, comments) {
         title = document.getElementById('articleTitleInput').value;
         description = document.getElementById('articleDescriptionInput').value;
-        timestamp = getTimestamp();
+        timestamp = timestampService.getTimestamp();
         image = 'http://www.droid-life.com/wp-content/uploads/2016/03/android-n-wallpaper-1.jpg';
 
         var firebaseUser = firebase.auth().currentUser;
@@ -60,7 +59,6 @@ angular.module('techNews').controller('NewArticleController', function (getLogge
         }
     }
 
-
     function getCurrentUser() {
         getLoggedInUser.getUser().then(function (result) {
             // Connect back to the scope
@@ -69,33 +67,6 @@ angular.module('techNews').controller('NewArticleController', function (getLogge
                 vm.user = result;
             }, 0);
         });
-    }
-
-// Get Date
-    function getTimestamp() {
-        // Expected result = September 1, 2016 9:13:34 PM
-        var objToday = new Date(),
-            weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
-            dayOfWeek = weekday[objToday.getDay()],
-            domEnder = function () {
-                var a = objToday;
-                if (/1/.test(parseInt((a + "").charAt(0)))) return "th";
-                a = parseInt((a + "").charAt(1));
-                return 1 == a ? "st" : 2 == a ? "nd" : 3 == a ? "rd" : ""
-            }(),
-            dayOfMonth = today + ( objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder : objToday.getDate() + domEnder,
-            months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
-            curMonth = months[objToday.getMonth()],
-            curYear = objToday.getFullYear(),
-            curHour = objToday.getHours() > 12 ? objToday.getHours() - 12 : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours()),
-            curMinute = objToday.getMinutes() < 10 ? "0" + objToday.getMinutes() : objToday.getMinutes(),
-            curSeconds = objToday.getSeconds() < 10 ? "0" + objToday.getSeconds() : objToday.getSeconds(),
-            curMeridiem = objToday.getHours() > 12 ? "PM" : "AM";
-
-        var timestamp = curMonth + ' ' + dayOfMonth + ', ' + curYear + ' ' + curHour + ":" + curMinute + ":" + curSeconds + ' ' + curMeridiem;
-        var today = curHour + ":" + curMinute + "." + curSeconds + curMeridiem + " " + dayOfWeek + " " + dayOfMonth + " of " + curMonth + ", " + curYear;
-
-        return timestamp
     }
 
 });
