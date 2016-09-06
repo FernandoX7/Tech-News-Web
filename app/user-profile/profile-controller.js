3b505e2f011a0d5c1b75a113786815ae744703c7
fernandox7 <framirez1997@gmail.com>
2016-09-05 20:24:08 -0500

    Add service to get the logged in user more efficiently in the profile view
    Just plug in the service and find out if a user is logged in - includes a promise

--- app/user-profile/profile-controller.js
+++ app/user-profile/profile-controller.js
@@ -1,18 +1,26 @@
 /**
  * Created by fernandoramirez on 9/4/16.
  */
-angular.module('techNews').controller('ProfileController', function ($timeout) {
+angular.module('techNews').controller('ProfileController', function ($timeout, getLoggedInUser) {
 
     var vm = this;
 
     // Public functions
+    vm.init = init;
     vm.editProfile = editProfile;
     vm.saveChanges = saveChanges;
     vm.getCurrentUser = getCurrentUser;
 
     // Public properties
     vm.isEditingProfile = false;
-    vm.user = vm.getCurrentUser();
+    vm.user = {}; // Update the properties with the users - if they are logged in
+
+    // Initialize whatever needs to be initialized :p
+    init();
+
+    function init() {
+        getCurrentUser();
+    }
 
     function editProfile() {
         vm.isEditingProfile = true;
@@ -24,20 +32,12 @@ angular.module('techNews').controller('ProfileController', function ($timeout) {
     }
 
     function getCurrentUser() {
-        firebase.auth().onAuthStateChanged(function (user) {
-            if (user) {
-                // User is signed in.
-                // Now get the specific user from the db using userRef to get the right reference to them
-                var userRef = firebase.database().ref('users/' + user.uid);
-                userRef.on('value', function (snapshot) {
-                    user = snapshot.val();
-                    // console.log('A user is logged in', user);
-                    // Connect back to scope
-                    $timeout(function () {
-                        vm.user = user;
-                    }, 0);
-                });
-            }
+        getLoggedInUser.getUser().then(function (result) {
+            // Connect back to the scope
+            $timeout(function () {
+                console.log('Attempting to get the logged in user resulted in the following promise (user) ', result);
+                vm.user = result;
+            }, 0);
         });
     }
 
